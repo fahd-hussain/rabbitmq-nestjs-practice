b098afca03ee63009eba536ec7b6500d547e00d8
Fahad Hussain <fahd__hussain@hotmail.com>
2023-04-07 02:35:59 +0500

    Rabbit MQ setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,19 +6,61 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
     'plugin:prettier/recommended',
+    // 'plugin:tslint-consistent-codestyle',
   ],
   root: true,
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
   rules: {
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: 'variable',
+        format: ['UPPER_CASE', 'snake_case'],
+      },
+      {
+        selector: ['classMethod', 'class'],
+        format: ['PascalCase'],
+      },
+    ],
+    // '@typescript-eslint/naming-convention': [
+    //   'error',
+    //   {
+    //     selector: 'classMethod',
+    //     format: ['PascalCase'],
+    //   },
+    // ],
+    // '@typescript-eslint/naming-convention': [
+    //   'warn',
+    //   {
+    //     selector: 'functionVariable',
+    //     format: ['camelCase'],
+    //   },
+    // ],
+    // 'tslint-consistent-codestyle':[
+
+    // ],
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 0,
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    'require-await': 'off',
+    '@typescript-eslint/require-await': 'error',
+    '@typescript-eslint/no-floating-promises': 'error',
   },
 };
